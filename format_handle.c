9489f0a60ab36977c69d9a619f0eb864fb127379
Damilola <dyadegbola@outlook.com>
2023-05-30 19:05:31 -0700

    printf_project

--- format_handle.c
+++ format_handle.c
@@ -165,7 +165,7 @@ int handle_prec(va_list args, const char *modifier, char *index)
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
  */
-unsigned int (*handle_specifiers(const char *specifier))(va_list buffer_t *,
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 		unsigned char, int, int, unsigned char)
 {
 	int i;
